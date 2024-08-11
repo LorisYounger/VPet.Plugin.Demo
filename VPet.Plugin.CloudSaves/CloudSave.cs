cfded26b5bc030798098318cadca1b9f8b703a2b
ZouJin <zoujin.dev@exlb.org>
2024-08-11 17:57:18 +0800

    云存档插件

---
+++ VPet.Plugin.CloudSaves/CloudSave.cs
@@ -0,0 +1,29 @@
+﻿using LinePutScript.Converter;
+using System.Collections.Generic;
+using VPet_Simulator.Windows.Interface;
+using CloudSaves.Client;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace VPet.Plugin.CloudSaves
+{
+    public class CloudSave : MainPlugin
+    {
+        Setting Set;
+        public SavesClient SavesClient;
+        public Timer BackupTimer;
+        public CloudSave(IMainWindow mainwin) : base(mainwin)
+        {
+
+        }
+        public override void LoadPlugin()
+        {
+            Set = LPSConvert.DeserializeObject<Setting>(MW.Set["CloudSave"]);
+            SavesClient = new SavesClient(Set.ServerURL, MW.SteamID, Set.Passkey);
+
+            BackupTimer = new Timer(Set.BackupTime * 60 * 1000);
+        }
+
+        public override string PluginName => "CloudSave";
+    }
+}
---
+++ VPet.Plugin.CloudSaves/Setting.cs
@@ -0,0 +1,33 @@
+﻿using LinePutScript.Converter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPet.Plugin.CloudSaves
+{
+    public class Setting
+    {
+        /// <summary>
+        /// 服务器链接
+        /// </summary>
+        [Line]
+        public string ServerURL { get; set; }
+        /// <summary>
+        /// 链接秘钥(密码)
+        /// </summary>
+        [Line]
+        public ulong Passkey { get; set; }
+
+        /// <summary>
+        /// 定时备份时间(分钟)
+        /// </summary>
+        [Line]
+        public int BackupTime { get => backupTime; set => backupTime = Math.Min(24 * 60, Math.Max(5, value)); }
+
+        private int backupTime = 20;
+
+
+    }
+}
